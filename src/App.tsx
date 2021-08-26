7f2bb1835e1b6fd24827318892c9590b4334660d
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-08-26 15:43:57 -0300

    feat: Landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
@@ -10,8 +11,10 @@ function App() {
       <AuthContextProvider>
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:roomId" component={Room} />
+
+          <Route path="/admin/rooms/:roomId" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,15 @@ import '../styles/button.scss'
 
 type ButtonProps = {
   children: React.ReactNode;
+  isOutline?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, isOutline = false, ...props }: ButtonProps) {
   return (
-    <button className="button" {...props}>
+    <button
+      className={`button ${isOutline && 'outlined'}`}
+      {...props}
+    >
       {children}
     </button>
   )
